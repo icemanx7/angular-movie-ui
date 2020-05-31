067286d374b84b8f42f6ceb832f1a9885a34d957
richard.thomas <richard.thomas36@gmail.com>
2020-05-31 15:54:43 +0200

    added review validators

--- src/app/modules/feature-movie/components/movie-form/movie-form.component.ts
+++ src/app/modules/feature-movie/components/movie-form/movie-form.component.ts
@@ -3,7 +3,7 @@ import { Movie, MovieReview, ReviewForm } from '../../models/movies.models';
 import * as auth from '../../../feature-auth/reducers';
 import { Store } from '@ngrx/store';
 import { SubmitMovieReview } from '../../actions/movie.actions';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-form',
@@ -16,8 +16,8 @@ export class MovieFormComponent implements OnInit {
   movie: Movie
 
   reviewForm = new FormGroup({
-    review: new FormControl(''),
-    rating: new FormControl(''),
+    review: new FormControl('', Validators.required),
+    rating: new FormControl('', Validators.required),
   });
 
   constructor(
@@ -27,6 +27,9 @@ export class MovieFormComponent implements OnInit {
   ngOnInit() { }
 
   submitReview() {
+    if (!this.reviewForm.valid) {
+      return;
+    }
     const formValues = this.reviewForm.value as ReviewForm;
     const date = new Date();
     const testReview = { movieID: this.movie.id, review: formValues.review, rating: formValues.rating, reviewDate: date } as MovieReview;
