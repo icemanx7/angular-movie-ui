f56b2de43b3ef97f2bf2348932dec88fda5532da
Richard Thomas <richard.thomas36@gmail.com>
2021-03-07 17:51:26 +0200

    Update movie.actions.ts

--- src/app/modules/feature-movie/actions/movie.actions.ts
+++ src/app/modules/feature-movie/actions/movie.actions.ts
@@ -21,46 +21,7 @@ export enum ActionTypes {
     LIKE_MOVIE_SUCCESS = '[Movie Page] Like Movie Success',
     LIKE_MOVIE_FAILED = '[Movie Page] Like Movie Failed',
 
-    // LOGIN_REDIRECT = '[Auth] Login Redirect',
-    // LOGIN_DISPLAY_LOGOUT = '[Auth] Login Display Logout',
-    // LOGIN_RESET_DISPLAY_LOGOUT = '[Auth] Login Reset Display Logout',
-
-    // LOGOUT = '[Auth] Logout',
-    // LOGOUT_SUCCESS = '[Auth] Logout Success',
-    // LOGOUT_FAILED = '[Auth] Logout Failed',
-
-    // REDIRECT_EXTERNAL = '[Auth] Redirect external',
-    // REDIRECT_EXTERNAL_SUCCESS = '[Auth] Redirect External Success',
-    // REDIRECT_EXTERNAL_FAILED = '[Auth] Redirect External Failed',
-
-    // EXTEND_LOGOUT_TIMER = '[Auth] Extend Logout Timer',
-    // SHOW_LOGOUT_TIMER_DIALOG = '[Auth] Show Logout Timer Dialog',
-
-    // LOAD_ELIGIBILITY = '[Eligibility] Load',
-    // LOAD_ELIGIBILITY_SUCCESS = '[Eligibility] Load Success',
-    // LOAD_ELIGIBILITY_FAILED = '[Eligibility] Load Failed',
-
-    // LOAD_USER = '[User] Load',
-    // LOAD_USER_SUCCESS = '[User] Load Success',
-    // LOAD_USER_FAILED = '[User] Load Failed',
-
-    // CONTAINER_LOGIN = '[Auth] Container Login',
-    // CONTAINER_LOGIN_SUCCESS = '[Auth] Container Login Success',
-    // CONTAINER_LOGIN_FAILED = '[Auth] Container Login Failed',
-
-    // SAVE_JWTTOKEN = '[Auth] Save JWT Token',
-    // SAVE_JWTTOKEN_SUCCESS = '[Auth] Save JWT Token Success',
-    // SAVE_JWTTOKEN_FAILED = '[Auth] Save JWT Token Failed',
-
-    // ENCRYPT_PARTY_ID = '[Auth] Encrypt party Id',
-    // ENCRYPT_PARTY_ID_SUCCESS = '[Auth] Encrypt party Id Success',
-    // ENCRYPT_PARTY_ID_FAILED = '[Auth] Encrypt party Id Failed',
-
-    // SET_USER_ID = '[Auth] Set UserId',
-    // SET_USER_ID_SUCCESS = '[Auth] Set User Id Success',
-    // SET_USER_ID_FAILED = '[Auth] Set User Id Success',
-
-    // SET_CUSTOMS_INFO = '[Auth] Set Customs Information'
+  
 }
 
 export class LoadMoviesList implements Action {
@@ -85,19 +46,6 @@ export class LoadMoviesListFailed implements Action {
 
 }
 
-// export const LoadMoviesList = createAction(
-//     '[Movie Page] Load Movies'
-// );
-
-// export const LoadMoviesListSuccess = createAction(
-//     '[Movie Page] Load Movies Success',
-//     props<Movies>()
-// );
-
-// export const LoadMoviesListFailed = createAction(
-//     '[Movie Page] Load Movies Failed'
-// );
-
 export class SubmitMovieReview implements Action {
 
     readonly type = ActionTypes.SUBMIT_MOVIE_REVIEW;
@@ -119,22 +67,6 @@ export class SubmitMovieReviewFailed implements Action {
     constructor(public payload: any) { }
 }
 
-
-// export const SubmitMovieReview = createAction(
-//     '[Movie Page: Review Component] Submit Review',
-//     props<MovieReview & UserDetails>()
-// );
-
-// export const SubmitMovieReviewSuccess = createAction(
-//     '[Movie Page: Review Component] Submit Review Success',
-//     props<MovieReview>()
-// );
-
-
-// export const SubmitMovieReviewFailed = createAction(
-//     '[Movie Page: Review Component] Submit Review Failed',
-//     props<MovieReview>()
-// );
 
 export class LikeMovie implements Action {
 
