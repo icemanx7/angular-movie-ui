24e2f57a82ac7985f35fb9eb71aa7de37a93f7fc
richard.thomas <richard.thomas36@gmail.com>
2021-03-07 11:26:21 +0200

    reverted the movie item view to a card for demo styles

--- src/app/modules/feature-auth/interceptors/auth-interceptor.ts
+++ src/app/modules/feature-auth/interceptors/auth-interceptor.ts
@@ -1,19 +1,18 @@
-// import { AuthService } from '../auth.service';
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
 import * as auth from '../reducers';
 import { Store } from '@ngrx/store';
-import { first, flatMap } from 'rxjs/operators';
+import { first, mergeMap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private _store: Store<auth.AppState>) { }
+  constructor(private readonly _store: Store<auth.AppState>) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return this._store.select(auth.getJwtToken).pipe(
       first(),
-      flatMap(loginSecrets => {
+      mergeMap(loginSecrets => {
         if (loginSecrets.isLoggedIn) {
           const authReq = req.clone({ setHeaders: { Authorization: loginSecrets.jwtToken } });
           return next.handle(authReq);
